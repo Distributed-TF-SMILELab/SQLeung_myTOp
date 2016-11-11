1432d3e86ab47ca350ba6413b94263f16b14b94f
Shiloh SQ. Leung <zszx0259@me.com>
2016-11-12 03:16:03 +0800

    add the test of t2mat

--- myTOp_test.py
+++ myTOp_test.py
@@ -22,21 +22,27 @@ print('='*12, "Vectorization of tensor",  "="*12, "\n")
 T = np.array([[[1,2,3,4],[5,6,7,8]], [[9,10,11,12], [13,14,15,16]]])
 print("shape of tensor T : ", T.shape)
 print("show tensor T : \n", T)
-print("\nusing t2vec to vetorize the tensor T : \n    vec = t2vec(T)")
+print("\n-- using t2vec to vetorize the tensor T : \n      vec = t2vec(T)")
 vec = mto.t2vec(T)
 print("show vec :\n", vec)
 print("shape of vector vec : ", vec.shape, '\n')
+
+
+
 
 # calculate norm of tensor
 print("\n"*2, '='*12, "calculate norm of tensor", '='*12, "\n")
 T1 = np.array([[1,2,3], [4,5,6]])
 print("show tensor T1 : \n", T1)
 normT1 = mto.tnorm(T1)
-print("\nusing tnorm to calculate norm of tensor T1 : \n    normT1 = tnorm(T1)")
+print("\n-- using tnorm to calculate norm of tensor T1 : \n      normT1 = tnorm(T1)")
 print("\nnormT1 = ", normT1)
 print("\ncalculate in another way:")
 print("    normT1_2 = np.sqrt(sum(sum(T1*T1)))")
 print("\nnormT1_2 = ", np.sqrt(sum(sum(T1*T1))))
+
+
+
 
 # calculate Kronecker product of 2 matrices
 print("\n"*2, '='*12, "Kronecker product of 2 matrices", "="*12, "\n")
@@ -47,7 +53,7 @@ print("show matrix A1 :\n", A1)
 print("\nshape of matrix B1 : ", B1.shape)
 print("show matrix B1 :\n", B1)
 prodKron = mto.mKron(A1, B1)
-print("\nusing mKron to calculate Kronecker product of A1 and B1 : \n    prodKron = mKron(A1, B1)\n")
+print("\n-- using mKron to calculate Kronecker product of A1 and B1 : \n      prodKron = mKron(A1, B1)\n")
 print("shape of prodKron : ", prodKron.shape)
 print("prodKron = \n", prodKron)
 
@@ -63,10 +69,9 @@ print("show matrix A2 :\n", A2)
 print("\nshape of matrix B2 : ", B2.shape)
 print("show matrix B2 :\n", B2)
 prodKhaR = mto.mKhaR(A2, B2)
-print("\nusing mKhaR to calculate Khatri-Rao product of A2 and B2 : \n    prodKhaR = mKhaR(A2, B2)\n")
+print("\n-- using mKhaR to calculate Khatri-Rao product of A2 and B2 : \n      prodKhaR = mKhaR(A2, B2)\n")
 print("shape of prodKhaR : ", prodKhaR.shape)
 print("prodKhaR = \n", prodKhaR)
-
 
 
 
@@ -81,9 +86,30 @@ print("show matrix A3 :\n", A3)
 print("\nshape of matrix B3 : ", B3.shape)
 print("show matrix B3 :\n", B3)
 prodHada = mto.mHadamard(A3, B3)
-print("\nusing mHadamard to calculate Hadamard product of A3 and B3 : \n    prodHada = mHadamard(A3, B3)\n")
+print("\n-- using mHadamard to calculate Hadamard product of A3 and B3 : \n        prodHada = mHadamard(A3, B3)\n")
 print("shape of prodHada : ", prodHada.shape)
 print("prodHada = \n", prodHada)
+
+
+
+
+# transfer a tensor into a matrix
+print("\n"*2, '='*12, "Matricization of tensor", "="*12, "\n")
+T2 = np.array([[[1, 13], [4, 16], [7, 19], [10, 22]], [[2, 14], [5, 17], [8, 20], [11, 23]], [[3, 15], [6, 18], [9, 21], [12, 24]]])
+print("shape of matrix T2 : ", T2.shape)
+print("show tensor T2 :\n", T2)
+T2mat_1 = mto.t2mat(T2, 1)
+T2mat_2 = mto.t2mat(T2, 2)
+T2mat_3 = mto.t2mat(T2, 3)
+print("\n\n-- using t2mat to transfer the tensor T2 into matrix in mode-1: \n      T2mat_1 = t2mat(T2, 1)")
+print("\nshape of matrix T2mat_1 : ", T2mat_1.shape)
+print("show matrix T2mat_1 :\n", T2mat_1)
+print("\n\n-- using t2mat to transfer the tensor T2 into matrix in mode-2: \n      T2mat_2 = t2mat(T2, 2)")
+print("\nshape of matrix T2mat_2 : ", T2mat_2.shape)
+print("show matrix T2mat_2 :\n", T2mat_2)
+print("\n\n-- using t2mat to transfer the tensor T2 into matrix in mode-3: \n      T2mat_3 = t2mat(T2, 3)")
+print("\nshape of matrix T2mat_3 : ", T2mat_3.shape)
+print("show matrix T2mat_3 :\n", T2mat_3)
 
 
 print('='*50)
